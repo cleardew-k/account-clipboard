a7016afa8cf7ef7933d1a86dab7a864a7834ba43
unknown <cleardew@kbanknow.com>
2022-03-18 17:59:57 +0900

    update clipboard.js

--- clipboard.js
+++ clipboard.js
@@ -1,26 +1,29 @@
-var data = require("./INST_INFO");
-var INST_INFO = data.INST_INFO;
+const data = require("./INST_INFO");
+const INST_INFO = data.INST_INFO;
 
 // 키워드 추출을 위한 정규식 객체를 생성
 // 케이|K뱅크|국민|우리|하나 ...
 function getRegExp() {
-  var regText = "";
+  let regText = "";
 
-  for (var key in INST_INFO) {
-    regText += key + "|";
+  const keys = Object.keys(INST_INFO);
+
+  keys.sort( (a, b)  => {
+    return b.length - a.length;
+  }); // 길이가 긴 패턴순으로 정렬
+
+  for (let i = 0; i < keys.length; i++) {
+    regText += keys[i] + "|";
   }
 
   // 마지막에 위치한 "|" 제거한 후 정규식 객체 생성
-  return new RegExp(regText.substr(0, regText.length - 1), "gi");
+  return new RegExp(regText.slice(0, regText.length - 1), "gi");
 }
 
 // 정규식 패턴
-var regHyphenPtn = /(\-|\,)/g;
-var regBlankPtn = /(\n|\r|\s)/g;
-var regNumLen = /\d{7,15}/g;
-var regMoney = /\d+원/g;
-var regExpObj = getRegExp();
-var regClean = /[^a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]/g; // 영문, 숫자, 한글 이외 모두 제외
+const regExpObj = getRegExp();
+const regAcctNum = /(\d-\d+-\d+-?\d+)|(\d{1,8}\s\d+\s\d+\s?\d+)|(\d{7,15})/g;
+const regMoney = /([1-9]\d{0,6}\s{0,1}원)|((\d{1},)?\d{1,3},\d{3}\s{0,1}원)|((\d{1},)?\d{1,3},\d{3})|([1-9]\d{0,2}만원)/g;
 
 /**
  * 정규식을 활용하여 기관코드, 계좌번호, 금액을 추출하여 객체 형태로 반환
@@ -29,63 +32,63 @@ var regClean = /[^a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]/g; // 영문, 숫자, 한글 이�
  * @returns {Object}       {instCode(금융기관코드), instAccount(계좌번호), txAmt(금액)}
  */
 function getResult(text) {
-  var instName = ""; // 금융기관명 (테스트를 위해 임시 추가)
-  var instCode = "";
-  var instAccount = "";
-  var txAmt = "";
+  let instCode = "";
+  let instAccount = "";
+  let txAmt = "";
 
-  // 하이픈, 쉼표 제거
-  var adjustText = text.replace(regHyphenPtn, "");
+  // 개행 공백으로 교체
+  const noLineText = text.replace(/\n/g, " ");
 
   // 금액 추출
-  var moneyArr = adjustText.match(regMoney) || [];
+  const moneyArr = noLineText.match(regMoney) || [];
   if (moneyArr && moneyArr.length > 0) {
-    txAmt = moneyArr[0] || "";
+    if (moneyArr[0] && moneyArr[0].indexOf("만원") > 0) {
+      txAmt = String(Number(moneyArr[0].replace(/만원/g, "")) * 10000);
+    } else {
+      txAmt = moneyArr[0].replace(/[,원]/g, "") || "";
+    }
   }
 
-  // 금액 추출 후 금액. 공백, 개행 제거
-  var noMoneyText = adjustText
-    .replace(regMoney, "")
-    .replace(regBlankPtn, "")
-    .replace(regClean, "");
+  // 금액 추출 후 금액 공백으로 교체
+  const noMoneyText = noLineText.replace(regMoney, " ");
 
   // 계좌번호 추출
-  var acctArr = noMoneyText.match(regNumLen) || [];
-  acctArr && acctArr.sort((a, b) => b.length - a.length); // 길이순 정렬 (핸드폰 번호 등 우선도 낮추기 위해)
-  instAccount = acctArr[0] || "";
-  var instAccountIdx = noMoneyText.indexOf(instAccount);
+  const rawAcctArr = noMoneyText.match(regAcctNum) || [];
+  const rawAcctArrSorted = rawAcctArr.sort(function (a, b) {
+      return b.length - a.length;
+    }); // 길이순 정렬 (핸드폰 번호 등 우선도 낮추기 위해)
 
+  const cleanAcctArr = rawAcctArrSorted.map((acct) => acct.replace(/\D/g, ""));
+
+  instAccount = cleanAcctArr[0] || "";
   // 은행명 추출
-  var matchNames = noMoneyText.match(regExpObj) || [];
+  const matchNames = noMoneyText.match(regExpObj) || [];
 
   if (matchNames.length > 1 && instAccount !== "") {
     // 은행명으로 추정되는 키워드가 2개 이상이고 계좌번호가 있을 때
     // 계좌번호 인덱스와 가장 가까운 키워드를 은행명으로 추출
-    var minDistance = 1000; // 최대 클립보드 문자 길이 1000
-    for (var i = 0; i < matchNames.length; i++) {
-      var incodeAcctNumText = noMoneyText.replace(instAccount, "@"); // 인덱스 거리 계산의 정확도를 위해 한자리 문자로 교체
-      var incodeNameText = incodeAcctNumText.replace(matchNames[i], "#"); // 인덱스 거리 계산의 정확도를 위해 한자리 문자로 교체
-      var distance = Math.abs(
-        incodeNameText.indexOf("#") - incodeNameText.indexOf("@")
+    let minDistance = 1000; // 최대 클립보드 문자 길이 1000
+    const incodeAcctNumText = noMoneyText.replace(rawAcctArr[0], "α"); // 인덱스 거리 계산의 정확도를 위해 계좌번호를 구획문자로 교체
+    for (let i = 0; i < matchNames.length; i++) {
+      const incodeNameText = incodeAcctNumText.replace(matchNames[i], "β"); // 인덱스 거리 계산의 정확도를 위해 은행명을 구획문자로 교체
+      const distance = Math.abs(
+        incodeNameText.indexOf("α") - incodeNameText.indexOf("β")
       );
       if (distance < minDistance) {
         minDistance = distance;
-        var fstKeyName = matchNames[i] || "";
-        instName = fstKeyName;
+        const fstKeyName = matchNames[i] || "";
         instCode = INST_INFO[matchNames[i].toUpperCase()] || "";
       }
     }
   } else {
-    var fstKeyName = matchNames[0] || "";
-    instName = fstKeyName;
+    const fstKeyName = matchNames[0] || "";
     instCode = INST_INFO[fstKeyName.toUpperCase()] || "";
   }
 
   return {
-    // instName: instName, // 금융기관명 (테스트를 위해 임시 추가)
     instCode: instCode, // 금융기관 코드
     instAccount: instAccount, // 계좌번호
-    txAmt: txAmt, // 금액
+    txAmt: txAmt // 금액
   };
 }
 
